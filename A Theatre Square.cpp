5910a316c25ecf452459b809edb18386a72c3f0c
Moufee Al Doushari <doushari.dipto212@gmail.com>
2024-03-27 02:12:39 +0600

    Add files via upload

---
+++ A Theatre Square.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    long long int n,m,a;
+    cin>>n>>m>>a;
+    long long int x,y;
+    if(n%a==0) {
+        x=n/a;
+    }
+    else{
+        x=(n/a) +1;
+    }
+    if(m%a==0) {
+        y=m/a;
+    }
+    else{
+        y=(m/a) +1;
+    }
+    cout<<x*y<<endl;
+    return 0;
+}
