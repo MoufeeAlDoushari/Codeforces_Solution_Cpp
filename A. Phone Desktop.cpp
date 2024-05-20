65555cecb0b8e459257a7234fb4defb901f3c4d2
Moufee Al Doushari <doushari.dipto212@gmail.com>
2024-05-20 23:17:28 +0600

    Add files via upload

---
+++ A. Phone Desktop.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int x, y;
+        cin >> x >> y;
+        cout << max((y + 1) / 2, (x + 4 * y + 14) / 15) << endl;
+    }
+}
