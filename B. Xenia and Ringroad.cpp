702deb9e4f7cc8fe5e8a6249555fa06307c864cc
Moufee Al Doushari <doushari.dipto212@gmail.com>
2024-07-30 23:32:28 +0600

    Add files via upload

---
+++ B. Xenia and Ringroad.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n,m;
+	cin>>n;
+	cin>>m;
+	int current=1;
+    long long moves =0;
+	for(int i=0;i<m;i++){
+		int goal;
+		cin>>goal;
+		if(goal-current>=0){
+			moves+=goal-current;
+			 current=goal;
+		}
+		else {
+			moves+=goal-current+n;
+			current=goal;
+		}
+	}
+	cout<<moves;
+
+
+	return 0;
+}
