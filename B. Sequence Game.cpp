55209578cae66680e2aab9eb553607e418a3c656
Moufee Al Doushari <doushari.dipto212@gmail.com>
2026-03-10 22:01:30 +0600

    Add files via upload

---
+++ B. Sequence Game.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<long long> b(n);
+        for(int i = 0; i < n; i++)
+        {
+            cin >> b[i];
+        }
+
+        vector<long long> a;
+        a.push_back(b[0]);
+
+        for(int i=1; i<n; i++)
+        {
+            if(b[i] < b[i-1])
+            {
+                a.push_back(b[i]);
+            }
+            a.push_back(b[i]);
+        }
+
+        cout << a.size() << endl;
+
+        for(long long x : a)
+        {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
